ec1334a6a57f549609e43cbbc65abf12a7d39029
Gabriel B. Rubenovitch <gbranchaudrubenovitch@users.noreply.github.com>
2018-12-01 01:11:01 -0500

    day 1, part 2 done

--- d01.js
+++ d01.js
@@ -8,3 +8,21 @@ const inputLines = readAllLines("d01-input");
 
 let total = inputLines.reduce((sum, current) => sum += current * 1, 0);
 console.log("d01p1 - resulting frequency is", total);
+
+let repeatingFrequencyFound = false;
+let previousTotals = new Map();
+previousTotals.set(0, true);
+let runningTotal = 0;
+while (!repeatingFrequencyFound) {
+    for(let l of inputLines) {
+        runningTotal += l * 1;
+
+        if (previousTotals.get(runningTotal)) {
+            repeatingFrequencyFound = true;
+            break;
+        }
+        previousTotals.set(runningTotal, true)
+    }
+}
+
+console.log("d01p2 - 1st frequency reached twice is", runningTotal);
